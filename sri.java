a81ace6f3c8551e69cdc409964a76b49981ce15f
srikanth <srikanthinvent33@gmail.com>
2018-10-11 04:43:49 -0700

    first comment of sri.java

---
+++ sri.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class SComparator implements Comparator
+{
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		String s1=o1.toString();
+		String s2=o2.toString();
+		return -s1.compareTo(s2);
+	}
+	
+}
+
+public class sri
+{
+	public static void main(String[] args) throws Exception{
+
+		//look into ground
+		FileInputStream fis=new FileInputStream("mad.txt");
+		InputStreamReader isr=new InputStreamReader(fis);
+		BufferedReader br=new BufferedReader(isr);
+
+
+
+
+
+
+
+
+
+
