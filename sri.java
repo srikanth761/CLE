85adfe153b2a36e4b97726933e390b0d9766339d
srikanth761 <44097649+srikanth761@users.noreply.github.com>
2018-10-12 11:53:09 -0700

    update sri.java

--- sri.java
+++ sri.java
@@ -7,9 +7,11 @@ class SComparator
 	@Override
 	public int compare(Object o1, Object o2) {
 		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 		String s1=o1.toString();
 		String s2=o2.toString();
 		String s2=o2.toString();
+		return -s1.compareTo(s2);
 		return -s1.compareTo(s2);
 	}
 	
